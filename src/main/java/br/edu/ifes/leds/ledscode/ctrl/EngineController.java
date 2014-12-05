9842b02755dd84cecacdf0f91430353824bfaa68
Marcos Dias <marcos.a.melo.dias@gmail.com>
2014-12-05 14:53:33 -0200

    Atualizacao do projeto - remocao do pacote servico

--- src/main/java/br/edu/ifes/leds/ledscode/ctrl/EngineController.java
+++ src/main/java/br/edu/ifes/leds/ledscode/ctrl/EngineController.java
@@ -1,27 +1,145 @@
 package br.edu.ifes.leds.ledscode.ctrl;
 
-import model.ClassDiagram;
-import model.Model;
-import model.Specification;
+
+import model.*;
+import model.Class;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
 /**
- * Created by marcosdias on 30/11/14.
+ * @author Marcos Dias
  */
 public class EngineController {
+
     /**
-     * Gera o codigo baseado nas informações e modelos contidos na especificacao
+     * Metodo que cria um projeto estatico.
      *
-     * @param specification - Especificacao que contem informacoes e
-     *                      modelos necessarios para criar o projeto
-     * @return
+     * @return Retorna uma especificacao de um projeto.
      */
-    public boolean criarProjeto(Specification specification) {
-        ClassDiagram diagram = (ClassDiagram) specification.getHas().get(0);
-        this.metamodeltoNode((ClassDiagram) specification.getHas().get(0));
-        return true;
+    public Specification createStaticProject() {
+        ArrayList<String> languages = this.languages;
+        ArrayList<String> frameworks = this.getFrameworks(languages.get(0));
+        ArrayList<String> database = this.getDataBase(frameworks.get(0));
+        ArrayList<String> orms = this.getOrms(frameworks.get(0));
+        ArrayList<String> applicationTypes = this.getApplicationType(frameworks.get(0));
+
+
+        Specification specification = new Specification();
+        specification.setName("Projeto Estatico");
+        specification.setCreatedDate(Calendar.getInstance());
+
+        Feature feature = new Feature();
+        feature.setLanguage(languages.get(0));
+        feature.setFramework(frameworks.get(0));
+        feature.setDatabase(database.get(0));
+        feature.setOrm(orms.get(0));
+        feature.setApplicationType(applicationTypes.get(0));
+
+        specification.setFeature(feature);
+
+        ClassDiagram classDiagram = new ClassDiagram();
+        classDiagram.setName("BolicheDiagram");
+
+        specification.getModels().add(classDiagram);
+
+        // Classe Jogador
+        model.Class jogador = new model.Class();
+        jogador.setName("Jogador");
+        jogador.setAbstractClass(false);
+        jogador.getStereotypeClasses().add(StereotypeClass.ENTITY);
+        jogador.getStereotypeClasses().add(StereotypeClass.VIEW);
+
+        // Atributos da class Jogador
+        Attribute nomeJogador = new Attribute();
+        nomeJogador.setName("nome");
+
+        PrimitiveType primitiveTypeNomeJogador = new PrimitiveType();
+        primitiveTypeNomeJogador.setType(Primitive.STRING);
+        nomeJogador.setType(primitiveTypeNomeJogador);
+
+        jogador.getAttributes().add(nomeJogador);
+
+        //Classe Liga
+        model.Class liga = new Class();
+        liga.setName("Liga");
+        liga.setAbstractClass(false);
+        liga.getStereotypeClasses().add(StereotypeClass.ENTITY);
+        liga.getStereotypeClasses().add(StereotypeClass.VIEW);
+
+        //Atributos da classe Liga
+        Attribute nomeLiga = new Attribute();
+        nomeLiga.setName("nome");
+
+        PrimitiveType primitiveDataTypeNomeLiga = new PrimitiveType();
+        primitiveDataTypeNomeLiga.setType(Primitive.STRING);
+        nomeLiga.setType(primitiveDataTypeNomeLiga);
+
+        Attribute jogadores = new Attribute();
+        jogadores.setName("jogadores");
+        jogadores.setType(jogador);
+
+        liga.getAttributes().add(nomeLiga);
+        liga.getAttributes().add(jogadores);
+
+        //Add classes no diagrama
+        classDiagram.getClasses().add(jogador);
+        classDiagram.getClasses().add(liga);
+
+        return specification;
     }
 
-    private void metamodeltoNode(ClassDiagram model) {
-        //model.get
+
+    /**
+     * TODO - REMOVER ATRIBUTO E METODOS SEGUINTES
+     * Estes atributos e metodos seguintes, seram obitido de forma dinamica nas proximas versoes,
+     * portanto, por agora utilizaremos valores estaticos
+     */
+    private ArrayList<String> languages;
+    private HashMap<String, ArrayList<String>> frameworks;
+    private HashMap<String, ArrayList<String>> dataBase;
+    private HashMap<String, ArrayList<String>> orms;
+    private HashMap<String, ArrayList<String>> applicationTypes;
+
+    public ArrayList<String> getFrameworks(String language) {
+        return this.frameworks.get(language);
+    }
+
+    public ArrayList<String> getDataBase(String framework) {
+        return this.dataBase.get(framework);
+    }
+
+    public ArrayList<String> getOrms(String framework) {
+        return this.orms.get(framework);
+    }
+
+    public ArrayList<String> getApplicationType(String framework) {
+        return this.applicationTypes.get(framework);
+    }
+
+    public EngineController() {
+        this.languages = new ArrayList<String>();
+        this.languages.add("Java");
+
+        ArrayList<String> listFramewoksJava = new ArrayList<String>();
+        listFramewoksJava.add("SPRINGROO");
+        this.frameworks = new HashMap<String, ArrayList<String>>();
+        this.frameworks.put("Java", listFramewoksJava);
+
+        ArrayList<String> listDataBaseROO = new ArrayList<String>();
+        listDataBaseROO.add("HYPERSONIC_IN_MEMORY");
+        this.dataBase = new HashMap<String, ArrayList<String>>();
+        this.dataBase.put("SPRINGROO", listDataBaseROO);
+
+        ArrayList<String> listOrmsROO = new ArrayList<String>();
+        listOrmsROO.add("HIBERNATE");
+        this.orms = new HashMap<String, ArrayList<String>>();
+        this.orms.put("SPRINGROO", listOrmsROO);
+
+        ArrayList<String> listApplicationTypes = new ArrayList<String>();
+        listApplicationTypes.add("WEB");
+        this.applicationTypes = new HashMap<String, ArrayList<String>>();
+        this.applicationTypes.put("SPRINGROO", listApplicationTypes);
     }
 }
