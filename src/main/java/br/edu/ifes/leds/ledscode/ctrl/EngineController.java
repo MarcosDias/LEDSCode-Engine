6f5e8eea02c3850a99fc0020ead123c72173ce81
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-01-07 02:41:23 -0200

    Refatoracao - metodo que cria o projeto estatico agora esta na API

--- src/main/java/br/edu/ifes/leds/ledscode/ctrl/EngineController.java
+++ src/main/java/br/edu/ifes/leds/ledscode/ctrl/EngineController.java
@@ -3,7 +3,6 @@ package br.edu.ifes.leds.ledscode.ctrl;
 
 import ctrl.SpringRooController;
 import model.*;
-import model.Class;
 
 import javax.xml.bind.*;
 import java.io.File;
@@ -59,83 +58,6 @@ public class EngineController {
     }
 
     /**
-     * Metodo que cria um projeto estatico.
-     *
-     * @return Retorna uma especificacao de um projeto.
-     */
-    public Specification createStaticProject() {
-        ArrayList<String> languages = this.languages;
-        ArrayList<String> frameworks = this.getFrameworks(languages.get(0));
-        ArrayList<String> database = this.getDataBase(frameworks.get(0));
-        ArrayList<String> orms = this.getOrms(frameworks.get(0));
-        ArrayList<String> applicationTypes = this.getApplicationType(frameworks.get(0));
-
-        Specification specification = new Specification();
-        specification.setName("Projeto_Estatico");
-        specification.setCreatedDate(Calendar.getInstance());
-
-        Feature feature = new Feature();
-        feature.setLanguage(languages.get(0));
-        feature.setFramework(frameworks.get(0));
-        feature.setDatabase(database.get(0));
-        feature.setOrm(orms.get(0));
-        feature.setApplicationType(applicationTypes.get(0));
-
-        specification.setFeature(feature);
-
-        ClassDiagram classDiagram = new ClassDiagram();
-        classDiagram.setName("BolicheDiagram");
-
-        specification.setClassDiagram(classDiagram);
-
-        // Classe Jogador
-        model.Class jogador = new model.Class();
-        jogador.setName("Jogador");
-        jogador.setAbstractClass(false);
-        jogador.getStereotypeClasses().add(StereotypeClass.ENTITY);
-        jogador.getStereotypeClasses().add(StereotypeClass.VIEW);
-
-        // Atributos da class Jogador
-        Attribute nomeJogador = new Attribute();
-        nomeJogador.setName("nome");
-
-        PrimitiveType primitiveTypeNomeJogador = new PrimitiveType();
-        primitiveTypeNomeJogador.setType(Primitive.STRING);
-        nomeJogador.setType(primitiveTypeNomeJogador);
-
-        jogador.getAttributes().add(nomeJogador);
-
-        //Classe Liga
-        model.Class liga = new Class();
-        liga.setName("Liga");
-        liga.setAbstractClass(false);
-        liga.getStereotypeClasses().add(StereotypeClass.ENTITY);
-        liga.getStereotypeClasses().add(StereotypeClass.VIEW);
-
-        //Atributos da classe Liga
-        Attribute nomeLiga = new Attribute();
-        nomeLiga.setName("nome");
-
-        PrimitiveType primitiveDataTypeNomeLiga = new PrimitiveType();
-        primitiveDataTypeNomeLiga.setType(Primitive.STRING);
-        nomeLiga.setType(primitiveDataTypeNomeLiga);
-
-        Attribute jogadores = new Attribute();
-        jogadores.setName("jogadores");
-        jogadores.setType(jogador);
-
-        liga.getAttributes().add(nomeLiga);
-        liga.getAttributes().add(jogadores);
-
-        //Add classes no diagrama
-        classDiagram.getClasses().add(jogador);
-        classDiagram.getClasses().add(liga);
-
-        return specification;
-    }
-
-
-    /**
      * TODO - REMOVER ATRIBUTO E METODOS SEGUINTES
      * Estes atributos e metodos seguintes, seram obitido de forma dinamica nas proximas versoes,
      * portanto, por agora utilizaremos valores estaticos
@@ -145,6 +67,10 @@ public class EngineController {
     private HashMap<String, ArrayList<String>> dataBase;
     private HashMap<String, ArrayList<String>> orms;
     private HashMap<String, ArrayList<String>> applicationTypes;
+    
+    public ArrayList<String> getLanguages(){
+        return this.languages;
+    }
 
     public ArrayList<String> getFrameworks(String language) {
         return this.frameworks.get(language);
