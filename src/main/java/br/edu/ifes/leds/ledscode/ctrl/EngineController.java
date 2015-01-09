41776f5ffd0493c11fec16217bf68f64eb0adc88
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-01-09 15:11:40 -0200

    adicao no modelo statico um novo framework para java

--- src/main/java/br/edu/ifes/leds/ledscode/ctrl/EngineController.java
+++ src/main/java/br/edu/ifes/leds/ledscode/ctrl/EngineController.java
@@ -6,6 +6,7 @@ import model.*;
 
 import javax.xml.bind.*;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -21,7 +22,7 @@ public class EngineController {
      * @return returna true caso nao haja nenhum erro
      */
     // TODO - Projeto gerado estaticamente em Spring Roo
-    public boolean generateCodeProject(Specification specification) {
+    public boolean generateCodeProject(Specification specification) throws IOException {
 
         SpringRooController springROO = new SpringRooController();
         springROO.createProject(specification);
@@ -30,12 +31,12 @@ public class EngineController {
     }
 
     /**
-     * Metodo usado apos a escrita do arquivo xml
+     * Metodo usado apos a escrita do model.model.arquivo xml
      * necessario para "acordar" a engine e continuar o processo
-     * @param nomeArquivo Nome do arquivo que sera lido
+     * @param nomeArquivo Nome do model.model.arquivo que sera lido
      * @return returna true caso nao haja nenhum erro
      */
-    public boolean acordarEngine(String nomeArquivo) throws JAXBException {
+    public boolean acordarEngine(String nomeArquivo) throws JAXBException, IOException {
         Specification specification = (Specification) lerArquivoXML(nomeArquivo);
         
         this.generateCodeProject(specification);
@@ -94,22 +95,30 @@ public class EngineController {
 
         ArrayList<String> listFramewoksJava = new ArrayList<String>();
         listFramewoksJava.add("Spring_Roo");
+        listFramewoksJava.add("Forge");
         this.frameworks = new HashMap<String, ArrayList<String>>();
         this.frameworks.put("Java", listFramewoksJava);
 
         ArrayList<String> listDataBaseROO = new ArrayList<String>();
+        ArrayList<String> listDataBaseForge = new ArrayList<String>();
         listDataBaseROO.add("HYPERSONIC_IN_MEMORY");
+        listDataBaseForge.add("HSQLDB_IN_MEMORY");
         this.dataBase = new HashMap<String, ArrayList<String>>();
         this.dataBase.put("Spring_Roo", listDataBaseROO);
+        this.dataBase.put("Forge", listDataBaseForge);
 
         ArrayList<String> listOrmsROO = new ArrayList<String>();
+        ArrayList<String> listOrmsForge = new ArrayList<String>();
         listOrmsROO.add("HIBERNATE");
+        listOrmsForge.add("HIBERNATE");
         this.orms = new HashMap<String, ArrayList<String>>();
         this.orms.put("Spring_Roo", listOrmsROO);
+        this.orms.put("Forge", listOrmsForge);
 
         ArrayList<String> listApplicationTypes = new ArrayList<String>();
         listApplicationTypes.add("WEB");
         this.applicationTypes = new HashMap<String, ArrayList<String>>();
         this.applicationTypes.put("Spring_Roo", listApplicationTypes);
+        this.applicationTypes.put("Forge", listApplicationTypes);
     }
 }
